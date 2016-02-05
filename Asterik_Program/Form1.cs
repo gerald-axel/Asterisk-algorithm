635781d6c00a227f6682a0d2594bf93f74b1a414
gerald-axel <gerald_6502@hotmail.com>
2016-02-04 23:06:51 -0600

    GUI Try

---
+++ Asterik_Program/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asterik_Program
+{
+    public partial class Form1 : Form
+    {
+
+        //private DataGridView dataGridView1 = new DataGridView();
+        public int filas, columnas;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                DataTable tabla = new DataTable();
+
+                for (int i = 0; i < int.Parse(textBox1.Text); i++)  
+                    tabla.Columns.Add();
+                
+                for (int i = 0; i < int.Parse(textBox2.Text); i++)
+                    tabla.Rows.Add();
+
+                dataGridView1.DataSource = tabla;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
